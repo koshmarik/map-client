d0367bf041364d888eed6a7ebf21f85f3e1a5b54
Mariya <cat-light1987@mail.ru>
2015-12-05 14:49:15 +0300

    changed upload data script

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
-insert into map_point(id, name, description) values ('1', 'Name1', 'Description1')
-insert into map_point(id, name, description) values ('2', 'Name2', 'Description2')
+insert into map_point(id, name, latitude, longitude, description) values ('1', 'Name1', '55.763338', '37.565466', 'Description1')
+insert into map_point(id, name, latitude, longitude, description) values ('2', 'Name2', '55.744522', '37.616378', 'Description2')
+insert into map_point(id, name, latitude, longitude, description) values ('3', 'Name3', '55.780898', '37.642889', 'Description3')
+insert into map_point(id, name, latitude, longitude, description) values ('4', 'Name4', '55.793559', '37.435983', 'Description4')
